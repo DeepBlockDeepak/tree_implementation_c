e1fa925a6a6c92e8fe4e5cf888854c8cf4f09a9c
DeepBlockDeepak <jordan.medina1729@gmail.com>
2021-10-23 01:26:26 -0600

    figured out how to correctly print the tree. Also created bst.c, which starts off identical to tree.c. Right now it has an insert_number function which places a node within the st correctly.

--- bst.c
+++ bst.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdbool.h>
 
 // binary tree implementation
 
@@ -16,11 +17,11 @@ struct treenode_t* createnode(int val){
     result = malloc(sizeof(struct treenode_t));
     assert(result && "createnode failed");
 
-    if(result != NULL){
-        result->value = val;
-        result->left_child = NULL;
-        result->right_child = NULL;
-    }
+
+    result->value = val;
+    result->left_child = NULL;
+    result->right_child = NULL;
+
     return result;
 
 }
@@ -32,13 +33,12 @@ void printtabs(int numtabs){
         printf("\t");
         
     }
-
     return;
 }
 
 
 void printtree_rec(struct treenode_t *root, int level){
-    if (root == NULL){
+    if (!root){
         printtabs(level);
         printf("--- empty tree ---\n");
 
@@ -48,19 +48,18 @@ void printtree_rec(struct treenode_t *root, int level){
     printtabs(level);
     printf("Leaf = %d\n", root->value);
     printtabs(level);
-    printf("Left\n");
     
+    printf("left of %d\n", root->value);
     printtree_rec(root->left_child, level + 1);
     printtabs(level);
-    printf("Right\n");
 
-
+    printf("right of %d\n", root->value);
     printtree_rec(root->right_child, level + 1);
     printtabs(level);
 
-     
+    printf("\n");    
 
-    printf("done\n");
+    //printf("good?\n");
 
 }
 
@@ -70,15 +69,40 @@ void printtree(struct treenode_t *root){
     printtree_rec(root, 0);
 }
 
+
+bool insert_number(struct treenode_t **root, int val){
+    struct treenode_t **tmp = root;
+
+    if(!(*root)){
+        *root = createnode(val);
+        return true;
+    }
+
+    if(val == (**tmp).value){
+        printf("Could not insert %d. Tree is full.\n", val);
+        return false;
+    }
+
+    if(val < (*tmp)->value){
+        return insert_number(&(**tmp).left_child, val);
+    }
+
+    if(val > (*tmp)->value){
+        return insert_number(&(**tmp).right_child, val);
+    }
+
+}
+
+
 int main(){
     
     //int tabs = 1;
-    struct treenode_t *new_node = createnode(10);
+    struct treenode_t *new_node = createnode(1);
 
-    struct treenode_t *n2 = createnode(11);
-    struct treenode_t *n3 = createnode(12);
-    struct treenode_t *n4 = createnode(13);
-    struct treenode_t *n5 = createnode(14);
+    struct treenode_t *n2 = createnode(2);
+    struct treenode_t *n3 = createnode(3);
+    struct treenode_t *n4 = createnode(4);
+    struct treenode_t *n5 = createnode(5);
     struct treenode_t *n6 = createnode(6);    
 
     new_node->left_child = n2;
@@ -88,6 +112,8 @@ int main(){
     n3->right_child = n5;
 
     n5->left_child = n6;
+
+    insert_number(&new_node, 3);
 
     printtree(new_node);
 
